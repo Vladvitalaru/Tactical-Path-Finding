377e8ebb26b219a3496ee00955b1eed8b96f9bb3
Vlad <vladvitalaru@yahoo.com>
2022-12-05 14:32:28 -0800

    Dijkstras path finding

--- TacticalPathFinding.py
+++ TacticalPathFinding.py
@@ -5,6 +5,10 @@
 import sys
 import heapq as heap
 import math
+import time 
+totaltime = time.time_ns()
+
+
 '''
 Road: '.' cost: 1
 Grass: ',' cost: 2
@@ -20,8 +24,6 @@ as the tile type indicates (so moving diagonally into rocks costs 4.5 points).
 
 You will read in the input and determine the shortest path between the character '0' and the character '2'. 
 The final square must be the closest adjacent square to the 2 (not the 2 itself, two characters may not occupy the same tile).
-
-The priority queue / heap in Python is called "heapq"
 '''
 
 ''' Node class holding information about each tile '''
@@ -29,75 +31,93 @@ class Node():
     def __init__(self, tile, cost, row, col):
         self.tile = tile            #tile type
         self.cost = cost            #tile type cost
-        self.prev = None            #Previous node in path
         self.totalCost = math.inf   #initial totalCost
+        self.prev = None            #Previous node in path
         self.row = row              #row index
         self.col = col              #column index
         
     def __lt__(self, next):
-        return self.cost < next.cost
+        return self.totalCost < next.totalCost
         
 ''' Matrix class holding grid & Dijksras algorithm'''
 class Matrix():
     def __init__(self, grid, start):
-        self.grid = grid
-        self.start = start
-        # self.destination = destination
-        self.visited = []   #visited array
-        self.Dijksras()
+        self.grid = grid        #tile map
+        self.start = start      #start index
+        self.visited = []       #visited array
+        self.pq = []            #priority que
+        self.final = 0          #final path score
+        self.Dijksras()         #run Dijkras
+        self.Display(self.grid) #print final tile map
+        print(f'\nTotal Cost: {self.final}')
         
         
     def Dijksras(self):
         
-        self.visited.append(self.start) #Add starting node to visited
-        self.grid[self.start[0]][self.start[1]].totalCost = 0       #Set start node totalCost to 0
+        self.visited.append(self.grid[self.start[0]][self.start[1]]) #Add starting node to visited
         
-        pq = []
-        heap.heappush(pq, self.grid[self.start[0]][self.start[1]])  #Push start node onto heap
+        self.grid[self.start[0]][self.start[1]].totalCost = 0        #Set start node totalCost to 0
+
+        heap.heappush(self.pq, self.grid[self.start[0]][self.start[1]])  #Push start node onto heap
         
-        # print(self.grid[start[0]][start[1]].totalCost)
-        # visited.add(self.grid[start[0]][start[1]])
-        # print(visited[0].tile)
-        # neighbors = self.neighbors(self.grid, start[0], start[1])
-        
-        # neighbors = self.neighbors(self.grid, 3, 5)
-        while pq:
-            next = heap.heappop(pq)
-            self.visited.append(next)
-            
-            neighbors = self.Neighbors(next.row, next.col)
-        
-            print()
-            for x in neighbors:
-                print(x.tile, end=" ")
-            print()
+        while self.pq:
+            next = heap.heappop(self.pq)
+                
+            if next.tile == "2": #if we have found destination
+                self.final = next.totalCost
+                cursor = next.prev
+                while cursor.prev:  #loop backwards through prev for path
+                    if cursor.tile == "0": 
+                        return
+                    else:
+                        cursor.tile = "*"
+                        cursor = cursor.prev
+                    
+                   
+            self.Neighbors(next.row, next.col)
+        return 
         
     def Neighbors(self, row, col):
         original = (row, col)
-        print(original)
-        print()
-        result = []
-        for rowAdd in range(-1, 2):
+                
+        originalCost = self.grid[original[0]][original[1]].totalCost #totalCost of currentNode
+        
+        for rowAdd in range(-1, 2): #for rows around
             newRow = row + rowAdd
             
-            if newRow >= 0 and newRow <= len(self.grid)-1:
+            if newRow >= 0 and newRow <= len(self.grid)-1: #for cols around
+                
                 for colAdd in range(-1, 2):
                     newCol = col + colAdd
                     
-                    if newCol >= 0 and newCol <= len(self.grid)-1:
+                    if newCol >= 0 and newCol <= len(self.grid)-1: #if index within boundary
                         if newCol == col and newRow == row:
                             continue
                         
                         if self.grid[newRow][newCol] in self.visited: #if we have already visited, move on
                             continue
                         
-                        print(newRow, newCol) 
-                        result.append(self.grid[newRow][newCol])
-
-        return result
+                        if self.grid[newRow][newCol].cost < 0: #If other player occupies tile, skip
+                            continue
+                                                
+                        if (rowAdd and colAdd == -1) or (rowAdd and colAdd == 1): #if neighbor at NW or SE nodes
+                            newCost = originalCost + (self.grid[newRow][newCol].cost * 1.5)
+                            
+                        elif (rowAdd == 1 and colAdd == -1) or (rowAdd == - 1 and colAdd == 1): #if neighbor at SW or NE nodes 
+                            newCost = originalCost + (self.grid[newRow][newCol].cost * 1.5)
+                            
+                        else:
+                            newCost = originalCost + self.grid[newRow][newCol].cost
+                        
+                        self.grid[newRow][newCol].totalCost = newCost
+                        self.grid[newRow][newCol].prev = self.grid[original[0]][original[1]]
+                        
+                        self.visited.append(self.grid[newRow][newCol]) #Add to visited 
+                        heap.heappush(self.pq,self.grid[newRow][newCol]) #push tile onto heap
+        return
         
     ''' Print out the Matrix to STDOUT '''
-    def display(self, grid):
+    def Display(self, grid):
         for x in grid:
             print()
             for y in x:
@@ -134,14 +154,9 @@ def main():
         
         grid.append(row)
         x = -1
-    # print(grid[start[0]][start[1]].row)
-    # print(f'tile = {grid[3][5].tile}')
-    # print(f'tile = {grid[0][0].tile}')
-    # print(f'col = {grid[0][0].col}')
-    # print(f'row = {grid[0][8].row}', f'col = {grid[0][8].col}')
 
-    matrix = Matrix(grid, start)
-    matrix.display(grid)
-
+    Matrix(grid, start)
+    time_taken_in_microseconds = ( time.time_ns() - totaltime ) / 1000.0 
+    print(f"Time taken in microseconds: {time_taken_in_microseconds} ", end = " ")
 if __name__ == '__main__':
     main()
