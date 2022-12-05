fa521872f77cdfa5545524654393083a9a59cc0a
Vlad <vladvitalaru@yahoo.com>
2022-12-04 21:51:16 -0800

    Finding adjacent nodes

--- TacticalPathFinding.py
+++ TacticalPathFinding.py
@@ -3,6 +3,8 @@
 #HW 5 Tactical Path Finding
 
 import sys
+import heapq as heap
+import math
 '''
 Road: '.' cost: 1
 Grass: ',' cost: 2
@@ -24,16 +26,75 @@ The priority queue / heap in Python is called "heapq"
 
 ''' Node class holding information about each tile '''
 class Node():
-    def __init__(self, tile, cost):
-        self.tile = tile
-        self.cost = cost
+    def __init__(self, tile, cost, row, col):
+        self.tile = tile            #tile type
+        self.cost = cost            #tile type cost
+        self.prev = None            #Previous node in path
+        self.totalCost = math.inf   #initial totalCost
+        self.row = row              #row index
+        self.col = col              #column index
+        
+    def __lt__(self, next):
+        return self.cost < next.cost
         
 ''' Matrix class holding grid & Dijksras algorithm'''
 class Matrix():
-    def __init__(self, grid, start, destination):
+    def __init__(self, grid, start):
         self.grid = grid
         self.start = start
-        self.destination = destination
+        # self.destination = destination
+        self.visited = []   #visited array
+        self.Dijksras()
+        
+        
+    def Dijksras(self):
+        
+        self.visited.append(self.start) #Add starting node to visited
+        self.grid[self.start[0]][self.start[1]].totalCost = 0       #Set start node totalCost to 0
+        
+        pq = []
+        heap.heappush(pq, self.grid[self.start[0]][self.start[1]])  #Push start node onto heap
+        
+        # print(self.grid[start[0]][start[1]].totalCost)
+        # visited.add(self.grid[start[0]][start[1]])
+        # print(visited[0].tile)
+        # neighbors = self.neighbors(self.grid, start[0], start[1])
+        
+        # neighbors = self.neighbors(self.grid, 3, 5)
+        while pq:
+            next = heap.heappop(pq)
+            self.visited.append(next)
+            
+            neighbors = self.Neighbors(next.row, next.col)
+        
+            print()
+            for x in neighbors:
+                print(x.tile, end=" ")
+            print()
+        
+    def Neighbors(self, row, col):
+        original = (row, col)
+        print(original)
+        print()
+        result = []
+        for rowAdd in range(-1, 2):
+            newRow = row + rowAdd
+            
+            if newRow >= 0 and newRow <= len(self.grid)-1:
+                for colAdd in range(-1, 2):
+                    newCol = col + colAdd
+                    
+                    if newCol >= 0 and newCol <= len(self.grid)-1:
+                        if newCol == col and newRow == row:
+                            continue
+                        
+                        if self.grid[newRow][newCol] in self.visited: #if we have already visited, move on
+                            continue
+                        
+                        print(newRow, newCol) 
+                        result.append(self.grid[newRow][newCol])
+
+        return result
         
     ''' Print out the Matrix to STDOUT '''
     def display(self, grid):
@@ -57,23 +118,29 @@ def main():
     grid = [] #Grid 
     y = -1 #row
     x = -1 #col
+    
     for line in sys.stdin.readlines():
         y += 1
         row = []
         for tile in line.split():
+            x += 1
             if tile == "0":
                 start = (y, x) #index of start (row, col)
             elif tile == "2": 
                 destination = (y, x) #index of destination (row, col)
                 
-            x += 1
-            node = Node(tile, tiles[tile])
+            node = Node(tile, tiles[tile], y, x)
             row.append(node)
         
         grid.append(row)
-        x = 0
-        
-    matrix = Matrix(grid, start, destination)
+        x = -1
+    # print(grid[start[0]][start[1]].row)
+    # print(f'tile = {grid[3][5].tile}')
+    # print(f'tile = {grid[0][0].tile}')
+    # print(f'col = {grid[0][0].col}')
+    # print(f'row = {grid[0][8].row}', f'col = {grid[0][8].col}')
+
+    matrix = Matrix(grid, start)
     matrix.display(grid)
 
 if __name__ == '__main__':
